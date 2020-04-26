6005f661046edbdae01ca52360a166b85b751a75
Vodinhphuc1234 <64115737+Vodinhphuc1234@users.noreply.github.com>
2020-04-26 10:38:52 +0700

    done login and show menu

--- project/project/Manager.h
+++ project/project/Manager.h
@@ -2,7 +2,7 @@
 #define _MANAGER_H_
 #include <iostream>
 #include <fstream>
-#include <cstring>
+#include <string>
 using namespace std;
 struct Date {
     int date, month, year;
@@ -12,6 +12,10 @@ struct Information {
     string password;
     string fullname;
     Date dob;
+    int male;//1:male,0:female
     int type; // 0: staff; 1: lecturer; 2: student
+    int active;// 0:unactive; 1:active
 };
+int login(fstream& fstu, fstream& fsta, fstream& flec);
+
 #endif
--- project/project/main.cpp
+++ project/project/main.cpp
@@ -1,6 +1,33 @@
+#include "Manager.h"
 #include <iostream>
 using namespace std;
 int main()
 {
-	return 0;
+	fstream fstu, fsta, flec;
+	int num=login(fstu, fsta, flec);
+	if (num == 0)
+	{
+		int choice;
+		show_menu_staff();
+		cout << "Choose option: ";
+		cin >> choice;
+		switch(choice)
+
+	}
+	else if (num == 1)
+	{
+		int choice;
+		show_menu_lecture();
+		cout << "Choose option: ";
+		cin >> choice;
+		switch (choice)
+	}
+	else
+	{
+		int choice;
+		show_menu_student;
+		cout << "Choose option: ";
+		cin >> choice;
+		switch (choice)
+	}
 }
